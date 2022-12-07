627c15722ad5f31f36fd038531aa74b2a9ed8250
Ben Rubenstein <ben@rube.pw>
2022-12-06 21:53:52 -0500

    remove tests from day05

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -76,20 +76,3 @@ fn main() {
     advent_of_code::solve!(1, part_one, input);
     advent_of_code::solve!(2, part_two, input);
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_one(&input).as_deref(), Some("CMZ"));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_two(&input), None);
-    }
-}
